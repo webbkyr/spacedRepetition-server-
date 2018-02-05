4bd5afb6800f7597bb1c054c473408238735c202
Kayla Webb <kaylarwebb@gmail.com>
2018-02-05 16:27:07 -0500

    Create question schema

---
+++ questions/models.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const QuestionSchema = mongoose.schema({
+  question: { type: String },
+  answer: { type: String }
+});
+
+const Question = mongoose.model('Question', QuestionSchema);
+
+module.exports = { Question };
