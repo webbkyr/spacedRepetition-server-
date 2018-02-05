bdc7d2ba93a3bc1a7d2d8ecb0266ce50b698ca88
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-05 12:19:04 -0800

    removed or statement in the createauthtoken function in the router

--- auth/router.js
+++ auth/router.js
@@ -9,7 +9,7 @@ const config = require('../config');
 const router = express.Router();
 
 const createAuthToken = function(user) {
-  return jwt.sign({user}, config.JWT_SECRET || 'gggg', {
+  return jwt.sign({user}, config.JWT_SECRET, {
     subject: user.username,
     expiresIn: config.JWT_EXPIRY,
     algorithm: 'HS256'
