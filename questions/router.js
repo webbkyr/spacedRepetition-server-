619c2ed8faff978dd99d50ab36051cdb48ccca7f
Kayla Webb <kaylarwebb@gmail.com>
2018-02-08 12:46:49 -0500

    Clean up comments and unsused code

--- questions/router.js
+++ questions/router.js
@@ -15,29 +15,14 @@ router.get('/dashboard', jwtAuth, (req, res) => {
 });
 
 router.get('/questions', jwtAuth, (req, res) => {
-  // console.log('user', req.session)
-  //create a user's session and attach the queue to the user session
-  // const user = req.user;
-  // User.findById(user.id)
-  //   .then(user => {
-  //     user.performance.forEach(question => questionQueue.enqueue(question));
-  //     // console.log('USER QUESTION QUEUE', questionQueue)
-  //     return res.json(helpers.peek(questionQueue));
-  //   })
-  //   .catch(err => {
-  //     res.status(500).json({code: 500, message: 'Something went wrong'});
-  //   });
+ 
   User.findById(req.user.id) 
     .then(user => {
-      // console.log('IN QUESTIONS ENDPOINT', user.head)
       res.json(user.performance[user.head]);
     });
 });
 
 router.get('/next', jwtAuth, (req, res) => {
-  // console.log('Next question', questionQueue.first.data);
-  // res.json(helpers.peek(questionQueue));
-  //return the next word in perf arr
   User.findById(req.user.id) 
     .then(user => {
       // console.log('USER HEAD IN NEXT ENDPOINT>>>>>>', user.head)
@@ -46,19 +31,14 @@ router.get('/next', jwtAuth, (req, res) => {
 });
 
 router.post('/responses', jwtAuth, jsonParser,(req, res) => {
-  const { uid, question, response } = req.body;
-  // res.json(response);
-  console.log(req.user);
+  const { response } = req.body;
   User.findById(req.user.id)
     .then(user => {
-      // console.log(user)
       user.performance.response = response;
       user.tail = (user.performance.length -1);
-      console.log('TAIL', user.tail);
 
       const answeredQuestionIndex = user.head;
       const answeredQuestion = user.performance[answeredQuestionIndex];
-      console.log('ANSWERED Q', answeredQuestion);
 
       if (user.head >= user.tail) {
         user.head = 0;
@@ -66,7 +46,6 @@ router.post('/responses', jwtAuth, jsonParser,(req, res) => {
       else {
         user.head = user.head+1;
       }
-      console.log('HEAD', user.head);
 
       answeredQuestion.next = user.tail;
 
